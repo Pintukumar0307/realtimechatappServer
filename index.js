816d52202b2197f2bbb81ba28bc8d74b55e6085e
Pintu kumar <84172057+Pintukumar0307@users.noreply.github.com>
2023-05-14 17:38:22 +0530

    3rd commit

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const cors=require('cors');
 const socketIo=require('socket.io');
 
 const app=express();
-const port=5000|| process.env.PORT;
-
+const port=5000 || process.env.PORT;
 
 const users =[{}];
 
@@ -15,9 +14,7 @@ app.get("/",(req,res)=>{
     res.send("Hell Its working");
 })
 
-
 const server=http.createServer(app);
-
 
 const io=socketIo(server);
 
@@ -32,21 +29,15 @@ io.on("connection",(socket)=>{
         socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]} has left the chat`})
     })
 
-    
-
     socket.on("message",({message,id})=>{
            io.emit("sendMessage",{user:users[id],message,id})
     })
-
 
     socket.on('disconnet',({user})=>{
         users[socket.id]=user;
         socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]} has left the chat`})
         console.log("user left")
     })
-    
-
-    
 
 })
 
@@ -54,51 +45,3 @@ server.listen(port,()=>{
     console.log(`server is working on ${port}` );
 })
 
-
-
-
-
-// const http=require("http");
-// const express =require("express");
-// const cors = require("cors");
-// const socketIO = require("socket.io");
-
-// const app=express();
-// const port= 5000 ||process.env.PORT ;
-
-
-// const users=[{}];
-
-// app.use(cors());
-// app.get("/",(req,res)=>{
-//     res.send("HELL ITS WORKING");
-// })
-
-// const server=http.createServer(app);
-
-// const io=socketIO(server);
-
-// io.on("connection",(socket)=>{
-//     console.log("New Connection");
-
-//     socket.on('joined',({user})=>{
-//           users[socket.id]=user;
-//           console.log(`${user} has joined `);
-//           socket.broadcast.emit('userJoined',{user:"Admin",message:` ${users[socket.id]} has joined`});
-//           socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]} `})
-//     })
-
-//     socket.on('message',({message,id})=>{
-//         io.emit('sendMessage',{user:users[id],message,id});
-//     })
-
-//     socket.on('disconnect',()=>{
-//           socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
-//         console.log(`user left`);
-//     })
-// });
-
-
-// server.listen(port,()=>{
-//     console.log(`Working`);
-// })
